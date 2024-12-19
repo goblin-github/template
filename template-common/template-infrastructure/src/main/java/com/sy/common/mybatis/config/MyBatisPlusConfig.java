7ef4feeece7459700007f72170e238c7218e64bf
Monster <wpeng078@126.com>
2024-12-19 15:15:19 +0800

    feat: 版本升级

--- template-common/template-infrastructure/src/main/java/com/sy/common/mybatis/config/MyBatisPlusConfig.java
+++ template-common/template-infrastructure/src/main/java/com/sy/common/mybatis/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.sy.common.mybatis.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @version v1.0
  */
 @Configuration
+@MapperScan(basePackages = "com.sy.**.mapper")
 public class MyBatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
